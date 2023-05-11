adcd4c24e20879928a77c9ea0e7bf6a80d3684c0
Bernd Meissner <s80652@bht-berlin.de>
2023-05-11 18:21:48 +0200

    Add test infrastructure

---
+++ src/test/java/com/dyna/model/ExampleClassTest.java
@@ -0,0 +1,12 @@
+package com.dyna.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExampleClassTest {
+
+    @Test
+    void doSomething() {
+    }
+}
