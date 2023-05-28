c9e175e1716b096bcf501ed1257a84f371a29109
Bernd Meissner <bernd.meissner@gmx.de>
2023-05-28 11:41:57 +0200

    Refactor PlayerController

--- src/main/java/com/dynamjb/controller/PlayerController.java
+++ src/main/java/com/dynamjb/controller/PlayerController.java
@@ -11,14 +11,31 @@ public class PlayerController {
         this.contolledPlayer = playerToControl;
     }
 
-    public void increaseXPos(){
-        double dx = contolledPlayer.getSpeed();
-        contolledPlayer.setXPosition(contolledPlayer.getXPosition() + dx);
+    public Player getPlayer(){
+      return this.contolledPlayer;
     }
-    public void decreaseXPos(){
-        double dx = contolledPlayer.getSpeed();
-        contolledPlayer.setXPosition(contolledPlayer.getXPosition() - dx);
+
+    public void clearVelocity(){
+        contolledPlayer.setVelocityX(0);
+        contolledPlayer.setVelocityY(0);
     }
+    public void setVelocity(double vertical, double horizontal){
+        contolledPlayer.setVelocityY(vertical);
+        contolledPlayer.setVelocityX(horizontal);
+    }
+
+    public void updatePlayer(){
+        return;
+    }
+
+//    public void increaseXPos(){
+//        double dx = contolledPlayer.getSpeed();
+//        contolledPlayer.setXPosition(contolledPlayer.getXPosition() + dx);
+//    }
+//    public void decreaseXPos(){
+//        double dx = contolledPlayer.getSpeed();
+//        contolledPlayer.setXPosition(contolledPlayer.getXPosition() - dx);
+//    }
 
 //    public void increaseYPos(){
 //        double dy = contolledPlayer.getSpeed();
@@ -28,12 +45,6 @@ public class PlayerController {
 //        double dy = contolledPlayer.getSpeed();
 //        contolledPlayer.setYPosition(contolledPlayer.getYPosition() - dy);
 //    }
-    public void clearVelocity(){
-        contolledPlayer.setVelocityX(0);
-        contolledPlayer.setVelocityY(0);
-    }
-    public void setVelocity(double vertical, double horizontal){
-        contolledPlayer.setVelocityY(vertical);
-        contolledPlayer.setVelocityX(horizontal);
-    }
+
+
 }
